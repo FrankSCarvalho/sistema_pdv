613cd19509a0443d8e7d780e5bd636b157b8f099
Frank Carvalho <65919656+FrankSCarvalho@users.noreply.github.com>
2026-01-14 09:11:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from telas.tela_login import TelaLogin
 from telas.tela_principal import TelaPrincipal
 
 if __name__ == "__main__":
-    app = TelaPrincipal()
-    app.mainloop()
+    # Abre a tela de login
+    tela_login = TelaLogin()
+    tela_login.mainloop()
+    
+    # Se o login foi bem-sucedido, abre a tela principal
+    usuario_autenticado = tela_login.get_usuario_autenticado()
+    
+    if usuario_autenticado:
+        app = TelaPrincipal(usuario_logado=usuario_autenticado)
+        app.mainloop()
